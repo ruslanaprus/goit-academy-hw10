8ab52df1ca2863c2e355d04ff34168e75354d9d2
ruslana <ruslana.prus@ukr.net>
2024-06-02 15:55:01 +0200

    collection of uniqueWords

--- src/main/java/org/example/wordcounter/WordFrequencyCounter.java
+++ src/main/java/org/example/wordcounter/WordFrequencyCounter.java
@@ -1,27 +1,36 @@
 package org.example.wordcounter;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.nio.file.Path;
+import java.util.*;
 
 public class WordFrequencyCounter {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         String filePath = "src/main/resources/words.txt";
-
         System.out.println(readWordsFromFile(filePath));
+
     }
 
-    public static List<String> readWordsFromFile(String filePath) {
-        try {
-            List<String> lines = Files.readAllLines(Paths.get(filePath));
-            return new ArrayList<>(lines);
+    protected static Set<String> readWordsFromFile(String filePath) {
+        Path path = Path.of(filePath);
+        Set<String> uniqueWords = new HashSet<>();
+
+        try (BufferedReader reader = Files.newBufferedReader(path, StandardCharsets.UTF_8)) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] words = line.toLowerCase().split("\\s+");
+                for (String word : words) {
+                    word = word.replaceAll("[\"!?,.]", "");
+                    uniqueWords.add(word);
+                }
+            }
         } catch (IOException e) {
-            e.printStackTrace();
+            throw new RuntimeException(e);
         }
-        return Collections.emptyList();
+        return uniqueWords;
     }
 }
