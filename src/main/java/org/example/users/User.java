dd46a5995392a7e325daf51758d1cb42db01c0a5
ruslana <ruslana.prus@ukr.net>
2024-06-02 14:22:40 +0200

    javadocs for User, description of package

--- src/main/java/org/example/users/User.java
+++ src/main/java/org/example/users/User.java
@@ -2,23 +2,51 @@ package org.example.users;
 
 import java.util.Objects;
 
+/**
+ * The {@code User} class represents a user with a name and an age.
+ * This class provides methods to access the user's details, as well as
+ * methods to compare users and obtain a string representation of the user.
+ */
 public class User {
     private String name;
     private int age;
 
+    /**
+     * Constructs a new {@code User} with the specified name and age.
+     *
+     * @param name The name of the user.
+     * @param age  The age of the user.
+     */
     public User(String name, int age) {
         this.name = name;
         this.age = age;
     }
 
+    /**
+     * Returns the name of the user.
+     *
+     * @return The name of the user.
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Returns the age of the user.
+     *
+     * @return The age of the user.
+     */
     public int getAge() {
         return age;
     }
 
+    /**
+     * Compares this user to the specified object. The result is {@code true} if and only if the
+     * argument is not {@code null} and is a {@code User} object that has the same name and age as this object.
+     *
+     * @param obj The object to compare this {@code User} against.
+     * @return {@code true} if the given object represents a {@code User} equivalent to this user, {@code false} otherwise.
+     */
     @Override
     public boolean equals(Object obj) {
         if (this == obj) return true;
@@ -27,11 +55,23 @@ public class User {
         return age == user.age && Objects.equals(name, user.name);
     }
 
+    /**
+     * Returns a hash code value for the user. This method is supported for the benefit of
+     * hash tables such as those provided by {@link java.util.HashMap}.
+     *
+     * @return A hash code value for this user.
+     */
     @Override
     public int hashCode() {
         return Objects.hash(name, age);
     }
 
+    /**
+     * Returns a string representation of the user. The string representation consists of
+     * the user's name and age in the format: [name, age].
+     *
+     * @return A string representation of the user.
+     */
     @Override
     public String toString() {
         return "[" + name + ", " + age + "]";
---
+++ src/main/java/org/example/users/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * This package, {@code org.example.users}, provides functionality to manage user data.
+ * It includes the ability to read user data from text files, convert it to JSON format,
+ * and write it to JSON files. This is useful for applications that need to handle user data
+ * in different formats for storage or transfer.
+ */
+package org.example.users;
