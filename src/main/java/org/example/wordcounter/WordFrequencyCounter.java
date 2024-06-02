d30cfefe35923f0e6a018701a90a9db978f83486
ruslana <ruslana.prus@ukr.net>
2024-06-02 16:36:14 +0200

    calculate word frequency from file

--- src/main/java/org/example/wordcounter/WordFrequencyCounter.java
+++ src/main/java/org/example/wordcounter/WordFrequencyCounter.java
@@ -11,26 +11,25 @@ public class WordFrequencyCounter {
 
     public static void main(String[] args) throws IOException {
         String filePath = "src/main/resources/words.txt";
-        System.out.println(readWordsFromFile(filePath));
+        System.out.println(calculateWordFrequencyFromFile(filePath));
 
     }
 
-    protected static Set<String> readWordsFromFile(String filePath) {
+    protected static Map<String, Integer> calculateWordFrequencyFromFile(String filePath) {
         Path path = Path.of(filePath);
-        Set<String> uniqueWords = new HashSet<>();
+        Map<String, Integer> wordFrequencyMap = new HashMap<>();
 
         try (BufferedReader reader = Files.newBufferedReader(path, StandardCharsets.UTF_8)) {
             String line;
             while ((line = reader.readLine()) != null) {
-                String[] words = line.toLowerCase().split("\\s+");
+                String[] words = line.toLowerCase().split("\\b[\\W-]+\\b");
                 for (String word : words) {
-                    word = word.replaceAll("[\"!?,.]", "");
-                    uniqueWords.add(word);
+                    wordFrequencyMap.put(word, wordFrequencyMap.getOrDefault(word, 0) + 1);
                 }
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        return uniqueWords;
+        return wordFrequencyMap;
     }
 }
